56fb3e9374ac3b57bbb2384358a5706bea50ea98
michaelcosimo <michael.cosimo@d3vtech.com>
2023-07-20 11:47:08 +0100

    new code

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/d3vtech/graphQL
 
 go 1.19
 
-require github.com/graphql-go/graphql v0.8.1 // indirect
+require (
+	github.com/graphql-go/graphql v0.8.1 // indirect
+	github.com/graphql-go/handler v0.2.3 // indirect
+	github.com/machinebox/graphql v0.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,102 +1,147 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 )
 
-func main() {
-	// Define GraphQL types
-	userType := graphql.NewObject(
-		graphql.ObjectConfig{
-			Name: "User",
-			Fields: graphql.Fields{
-				"id":    &graphql.Field{Type: graphql.ID},
-				"name":  &graphql.Field{Type: graphql.String},
-				"email": &graphql.Field{Type: graphql.String},
-			},
+// Book represents a book in the library
+type Book struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Author      string    `json:"author"`
+	Description string    `json:"description"`
+	Price       float64   `json:"price"`
+	IsAvailable bool      `json:"isAvailable"`
+	RentedBy    string    `json:"rentedBy,omitempty"`
+	RentedAt    time.Time `json:"rentedAt,omitempty"`
+}
+
+// Define GraphQL types for Book
+var bookType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Book",
+		Fields: graphql.Fields{
+			"id":          &graphql.Field{Type: graphql.String},
+			"title":       &graphql.Field{Type: graphql.String},
+			"author":      &graphql.Field{Type: graphql.String},
+			"description": &graphql.Field{Type: graphql.String},
+			"price":       &graphql.Field{Type: graphql.Float},
+			"isAvailable": &graphql.Field{Type: graphql.Boolean},
+			"rentedBy":    &graphql.Field{Type: graphql.String},
+			"rentedAt":    &graphql.Field{Type: graphql.DateTime},
 		},
-	)
+	},
+)
 
-	// Define root query
-	rootQuery := graphql.NewObject(
-		graphql.ObjectConfig{
-			Name: "Query",
-			Fields: graphql.Fields{
-				"user": &graphql.Field{
-					Type: userType,
-					Args: graphql.FieldConfigArgument{
-						"id": &graphql.ArgumentConfig{Type: graphql.ID},
+// Define the root query
+var rootQuery = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"book": &graphql.Field{
+				Type: bookType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.String,
 					},
-					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-						// Resolve user data based on the provided ID
-						id, ok := p.Args["id"].(string)
-						if !ok {
-							return nil, nil
-						}
-
-						// Retrieve user data from the database or other sources
-						// Here, we're returning dummy data for demonstration purposes
-						if id == "1" {
-							return map[string]interface{}{
-								"id":    "1",
-								"name":  "John Doe",
-								"email": "john@example.com",
-							}, nil
-						}
-
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					// Retrieve the book by ID
+					id, ok := p.Args["id"].(string)
+					if !ok {
 						return nil, nil
-					},
+					}
+
+					// Retrieve book data from the database or other sources
+					// Here, we're returning dummy data for demonstration purposes
+					return &Book{
+						ID:          id,
+						Title:       "Book Title",
+						Author:      "Book Author",
+						Description: "Book Description",
+						Price:       9.99,
+						IsAvailable: true,
+						RentedBy:    "John Doe",
+						RentedAt:    time.Now(),
+					}, nil
 				},
 			},
 		},
-	)
+	},
+)
 
+// Define the root mutation
+var rootMutation = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Mutation",
+		Fields: graphql.Fields{
+			"rentBook": &graphql.Field{
+				Type: bookType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.String,
+					},
+					"rentedBy": &graphql.ArgumentConfig{
+						Type: graphql.String,
+					},
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					// Retrieve the book by ID
+					id, ok := p.Args["id"].(string)
+					if !ok {
+						return nil, nil
+					}
+
+					// Retrieve additional input
+					rentedBy, _ := p.Args["rentedBy"].(string)
+
+					// Rent the book
+					book := &Book{
+						ID:          id,
+						Title:       "Book Title",
+						Author:      "Book Author",
+						Description: "Book Description",
+						Price:       9.99,
+						IsAvailable: false,
+						RentedBy:    rentedBy,
+						RentedAt:    time.Now(),
+					}
+
+					// Persist the rented book data in the database or other sources
+					// Here, we're simply returning the rented book for demonstration purposes
+					return book, nil
+				},
+			},
+		},
+	},
+)
+
+func main() {
 	// Define the GraphQL schema
 	schema, err := graphql.NewSchema(
 		graphql.SchemaConfig{
-			Query: rootQuery,
+			Query:    rootQuery,
+			Mutation: rootMutation,
 		},
 	)
 	if err != nil {
 		log.Fatalf("Failed to create GraphQL schema: %v", err)
 	}
 
-	// Handle GraphQL requests
-	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
-		// Parse the request body
-		var reqBody struct {
-			Query string `json:"query"`
-		}
-		if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
-			http.Error(w, "Invalid request", http.StatusBadRequest)
-			return
-		}
-
-		// Execute the GraphQL query
-		result := graphql.Do(graphql.Params{
-			Schema:        schema,
-			RequestString: reqBody.Query,
-		})
-
-		// Convert the result to JSON
-		jsonResult, err := json.Marshal(result)
-		if err != nil {
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
-			return
-		}
-
-		// Set the response headers
-		w.Header().Set("Content-Type", "application/json")
-
-		// Write the response
-		w.Write(jsonResult)
+	// Create a new GraphQL handler with the schema
+	h := handler.New(&handler.Config{
+		Schema:   &schema,
+		Pretty:   true,
+		GraphiQL: true,
 	})
 
-	// Start the server
+	// Serve the GraphQL endpoint
+	http.Handle("/graphql", h)
 	log.Println("Server is running on http://localhost:8080/graphql")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
